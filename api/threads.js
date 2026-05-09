8d229b81bfc2f548ad071ca5ed0fc59f26ea111c
Mark <mark@lafayettemc.com>
2026-05-09 19:58:17 -0400

    Fix: replace export default with module.exports in handler

--- api/threads.js
+++ api/threads.js
@@ -17,7 +17,7 @@ async function fetchMetData(title, artist) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");

045da2d6e2bfe62c1ddde5311cf9a8276f2676fd
art-thread <mark@lafayettemc.com>
2026-05-09 18:42:10 -0400

    Change module.exports to export default in handler

--- api/threads.js
+++ api/threads.js
@@ -149,7 +149,7 @@ async function fetchArtworkImage(title, artist) {
   - Ignore glass reflections glare and people in foreground.
   `;
 
-  module.exports = async function handler(req, res) {
+  export default async function handler(req, res) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
